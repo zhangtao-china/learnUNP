0fc766310dba46be17e1afe4194b2923214f7121
zhangtao <nupt_zt@126.com>
2018-02-01 23:21:49 +0800

    add udp part Makefile

--- ch8/client_udp_v2.c
+++ ch8/client_udp_v2.c
@@ -33,26 +33,31 @@ void dg_cli(FILE *fp, int sockfd, const sockaddr *pservaddr, socklen_t servlen)
     int n;
     char sendline[MAXLINE], recvline[MAXLINE + 1];
 
-    struct sockaddr_storage cliaddr;
+    struct sockaddr_storage replyaddr;
     socklen_t len;
     char buf[SOCKADDR_STR_BUF_LEN];
     while(Fgets(sendline, MAXLINE, fp) != NULL)
     {
         Sendto(sockfd, sendline, strlen(sendline), 0, pservaddr, servlen);
+
 /*
 问题：
-任何其他发送者都可以向本客户端的IP地址和端口发送数据，这些数据报将被客户读入并被认为是服务器的应答
+如果服务器是多宿的，该客户可能失败
+两个解决办法：
+1.得到由recvfrom返回的IP地址后，客户通过DNS中查找服务器的名字来验证该主机的域名
+2.UDP服务器给服务器主机上配置的每个IP地址创建一个套接字，用bind捆绑每个IP地址到各自的套接字，然后在
+  所有这些套接字上使用select，再从可读的套接字给出应答
 */
+        len = servlen;
+        n = Recvfrom(sockfd, recvline, MAXLINE, 0, (sockaddr *)&replyaddr, &len);
 
-        len = sizeof(cliaddr);
-        n = Recvfrom(sockfd, recvline, MAXLINE, 0, (sockaddr *)&cliaddr, &len);
-
-#if 0    
-        if(sock_ntop((sockaddr *)&cliaddr, len, buf, sizeof(buf)))
+        if(len != servlen || memcmp(pservaddr, &replyaddr, len) != 0)
         {
-            printf("[client_udp_v1] recv %d bytes from %s\n", n, buf);
+            printf("reply from %s (ignored)\n", 
+                   sock_ntop((sockaddr *)&replyaddr, len, buf, sizeof(buf)));
+            continue;
         }
-#endif
+
         recvline[n] = 0;
         Fputs(recvline, stdout);
     }
